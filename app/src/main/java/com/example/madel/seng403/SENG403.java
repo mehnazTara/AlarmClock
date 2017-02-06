a9143cc0a85ae00dbc3bc50a8a14f67bb4154ef4
QuinnBischoff <quinn.qb@gmail.com>
2017-02-06 13:31:26 -0700

    linked main activity to setAlarm

--- app/src/main/java/com/example/madel/seng403/SENG403.java
+++ app/src/main/java/com/example/madel/seng403/SENG403.java
@@ -1,5 +1,6 @@
 package com.example.madel.seng403;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,11 @@ public class SENG403 extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void goToSetAlarmActivity(View view)
+    {
+        Intent intent = new Intent(SENG403.this, SetAlarmActivity.class);
+        startActivity(intent);
     }
 }
