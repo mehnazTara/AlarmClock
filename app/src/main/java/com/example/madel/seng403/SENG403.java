87f487aa59dc41c2611de4adcb86ef30534a4d0a
QuinnBischoff <quinn.qb@gmail.com>
2017-02-06 15:27:42 -0700

    linked buttons in main activity to SetAlarm activity

--- app/src/main/java/com/example/madel/seng403/SENG403.java
+++ app/src/main/java/com/example/madel/seng403/SENG403.java
@@ -18,15 +18,7 @@ public class SENG403 extends AppCompatActivity {
         setContentView(R.layout.activity_seng403);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
     }
 
     @Override
--- app/src/main/java/com/example/madel/seng403/SetAlarmActivity.java
+++ app/src/main/java/com/example/madel/seng403/SetAlarmActivity.java
@@ -1,11 +1,14 @@
 package com.example.madel.seng403;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import java.util.Set;
 
 public class SetAlarmActivity extends AppCompatActivity {
 
@@ -15,15 +18,18 @@ public class SetAlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_set_alarm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void setAlarmButton(View view)
+    {
+        Intent intent = new Intent(SetAlarmActivity.this, SENG403.class);
+        startActivity(intent);
+    }
+
+    public void cancelAlarmButton(View view)
+    {
+        Intent intent = new Intent(SetAlarmActivity.this, SENG403.class);
+        startActivity(intent);
     }
 
 }
