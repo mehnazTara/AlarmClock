b92cfbf741110b15e1b65e0f92794b2926d845b3
mtarannu <mtarannu@ucalgary.ca>
2017-02-07 16:35:44 -0700

    able to set single alarm and alarm activation displayed  with toast message and vibration

--- app/src/main/java/com/example/madel/seng403/MainActivity.java
+++ app/src/main/java/com/example/madel/seng403/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,34 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
+
+
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_seng403, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void goToSetAlarmActivity(View view)
     {
         Intent intent = new Intent(MainActivity.this, SetAlarmActivity.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/example/madel/seng403/SetAlarmActivity.java
+++ app/src/main/java/com/example/madel/seng403/SetAlarmActivity.java
@@ -1,12 +1,28 @@
 package com.example.madel.seng403;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+/**
+ * This class deals with setting the alarm and activating the alarm
+ * @author Group 13
+ */
 
 public class SetAlarmActivity extends AppCompatActivity {
+
+    // private fields
+    private TimePicker pickerTime;
+    private  Button buttonSetAlarm;
+    private AlarmManager alarmManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +30,80 @@ public class SetAlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_set_alarm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        pickerTime = (TimePicker)findViewById(R.id.timePicker);
+
+        // getting the system alarm service
+        alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+
+        buttonSetAlarm = (Button)findViewById(R.id.set_alarm);
+
+
+        // set on click listener method for set button
+        buttonSetAlarm.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                // the reason for using two calenders for future extension
+                // for checking is current time was selected , for example not a time from past
+                Calendar calCurr = Calendar.getInstance();
+
+                String time = calCurr.getTime().toString();
+                Log.e("Log meesage: ", time);
+                Calendar calTarget = (Calendar) calCurr.clone();
+
+                // getting the time from timepicker and setting it to the caldendar
+                calTarget.set(Calendar.HOUR_OF_DAY, pickerTime.getHour());
+                calTarget.set(Calendar.MINUTE, pickerTime.getMinute());
+                calTarget.set(Calendar.SECOND, 0);
+                calTarget.set(Calendar.MILLISECOND, 0);
+
+
+                String time1 = calTarget.getTime().toString();
+
+                Log.e("Log message: ", "set time " + time1);
+
+
+                setAlarm(calTarget);
+
+            }});
     }
 
-    public void setAlarmButton(View view)
+
+    /*
+     *creates the intent, pendingIndent and sets time for alarmManager object
+     * @param calendar set to user specified time
+     */
+    private void setAlarm(Calendar targetCal){
+
+        // creating an intent associated with AlarmReceiver class
+        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+
+        // creating  a pending intent that delays the intent until the specified calender time is reached
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(SetAlarmActivity.this, 0, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // setting the alarm Manager to set alarm at exact time of the user chosen time
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, targetCal.getTimeInMillis(), pendingIntent);
+        goBacktoMain();
+    }
+
+
+    /*
+     * starts the MainActivity intent , i.e takes us back to main page
+     *
+     */
+    public void goBacktoMain()
     {
         Intent intent = new Intent(SetAlarmActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
+
+    /*
+     * takes us back to MainActivity
+     *
+     */
     public void cancelAlarmButton(View view)
     {
         Intent intent = new Intent(SetAlarmActivity.this, MainActivity.class);
