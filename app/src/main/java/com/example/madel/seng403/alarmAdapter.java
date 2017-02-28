a8779765a477268a1697d928a012c082842113e7
QuinnBischoff <quinn.qb@gmail.com>
2017-02-28 15:27:08 -0700

    cancel buttons exist but don't work

--- app/src/main/java/com/example/madel/seng403/alarmAdapter.java
+++ app/src/main/java/com/example/madel/seng403/alarmAdapter.java
@@ -1,11 +1,17 @@
 package com.example.madel.seng403;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -40,7 +46,7 @@ public class AlarmAdapter extends BaseAdapter {
         return alarmList.get(i).getID();
     }
 
-    @Override
+   /* @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         if(view==null)
         {
@@ -49,5 +55,29 @@ public class AlarmAdapter extends BaseAdapter {
         TextView name = (TextView) view.findViewById(android.R.id.text1);
         name.setText(alarmList.get(i).getHourString() + ":" + alarmList.get(i).getMinuteString());
         return view;
+    } */
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(view==null)
+        {
+            view = inflator.inflate(R.layout.rowlayout, viewGroup, false);
+        }
+        TextView name = (TextView) view.findViewById(R.id.list_item_string);
+        name.setText(alarmList.get(i).getHourString() + ":" + alarmList.get(i).getMinuteString());
+
+        Button cancelButton = (Button) view.findViewById(R.id.delete_btn);
+        cancelButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent cancelIntent = new Intent(context,AlarmReceiver.class);
+                PendingIntent cancelPendingIntent = PendingIntent.getBroadcast(context, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                Toast toast = Toast.makeText(context.getApplicationContext(), "Alarm Set!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        return view;
     }
+
 }
