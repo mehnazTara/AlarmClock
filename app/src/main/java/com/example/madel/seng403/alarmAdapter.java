9e6573ea727e7047a46f36ed47090b44c28f028a
madel1031@hotmail.com <madel1031@hotmail.com>
2017-03-14 14:45:27 -0600

    creating a new branch for testing purposes

--- app/src/main/java/com/example/madel/seng403/alarmAdapter.java
+++ app/src/main/java/com/example/madel/seng403/alarmAdapter.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,9 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton;
+import java.util.concurrent.TimeUnit;
 
 import java.util.ArrayList;
 
@@ -19,12 +23,15 @@ import static android.content.Context.ALARM_SERVICE;
 
 /**
  * Created by Quinn on 2017-02-27.
+ * Adapter for the list view of alarmlistfragment for formatting
  */
 
 public class AlarmAdapter extends BaseAdapter {
     Context context;
     LayoutInflater inflator;
     ArrayList<AlarmDBItem> alarmList;
+    int idforbuttonalarm;
+    long currentHour, currentMin;
 
     public AlarmAdapter(Context c, ArrayList<AlarmDBItem> alarms)
     {
@@ -48,17 +55,7 @@ public class AlarmAdapter extends BaseAdapter {
         return alarmList.get(i).getID();
     }
 
-   /* @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        if(view==null)
-        {
-            view = inflator.inflate(android.R.layout.simple_list_item_1, viewGroup, false);
-        }
-        TextView name = (TextView) view.findViewById(android.R.id.text1);
-        name.setText(alarmList.get(i).getHourString() + ":" + alarmList.get(i).getMinuteString());
-        return view;
-    } */
-
+/*
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         if(view==null)
@@ -67,20 +64,66 @@ public class AlarmAdapter extends BaseAdapter {
         }
         TextView name = (TextView) view.findViewById(R.id.list_item_string);
         name.setText(alarmList.get(i).getHourString() + ":" + alarmList.get(i).getMinuteString());
-
-        Button cancelButton = (Button) view.findViewById(R.id.delete_btn);
+        idforbuttonalarm = i;
+        final Button cancelButton = (Button) view.findViewById(R.id.delete_btn);
+        cancelButton.setTag(i);
         cancelButton.setOnClickListener(new View.OnClickListener(){
+            //functionality of the cancel button for a given alarm
+            //prevents the alarm from ringing before it goes off.
             @Override
             public void onClick(View v) {
+                cancelButton.setBackgroundColor(0xff0000);
+                //Log.e("Log message: ", "button id: " + cancelButton.getTag());
+                //Log.e("Log message: ", "alarm cancelled with id: " + alarmList.get((int) cancelButton.getTag()).getID());
                 Intent cancelIntent = new Intent(context,AlarmReceiver.class);
-                PendingIntent cancelPendingIntent = PendingIntent.getBroadcast(context, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                PendingIntent cancelPendingIntent = PendingIntent.getBroadcast(context, alarmList.get((int) cancelButton.getTag()).getID(), cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT);
                 AlarmManager am = (AlarmManager) context.getSystemService(ALARM_SERVICE);
                 am.cancel(cancelPendingIntent);
-                Toast toast = Toast.makeText(context.getApplicationContext(), "Alarm Set!", Toast.LENGTH_LONG);
+                Toast toast = Toast.makeText(context.getApplicationContext(), "Alarm Cancelled!", Toast.LENGTH_LONG);
                 toast.show();
             }
         });
         return view;
     }
+*/
 
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            view = inflator.inflate(R.layout.rowlayout, viewGroup, false);
+        }
+        TextView name = (TextView) view.findViewById(R.id.list_item_string);
+        name.setText(alarmList.get(i).getHourString() + ":" + alarmList.get(i).getMinuteString());
+        idforbuttonalarm = i;
+
+        currentHour = alarmList.get(i).getHour();
+        currentMin = alarmList.get(i).getMinute();
+
+        //Cancel Toggle Button
+        final ToggleButton cancelToggle = (ToggleButton) view.findViewById(R.id.cancel_btn);
+        cancelToggle.setTag(i);
+        cancelToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //Cancel toggle function
+                if (isChecked) {
+                    Intent cancelIntent = new Intent(context, AlarmReceiver.class);
+                    PendingIntent cancelPendingIntent = PendingIntent.getBroadcast(context, alarmList.get((int) cancelToggle.getTag()).getID(), cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    AlarmManager am = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+                    am.cancel(cancelPendingIntent);
+                    Toast toast = Toast.makeText(context.getApplicationContext(), "Alarm Set!", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                //Enable alarm toggle function
+                else {
+                    Intent enableIntent = new Intent(context, AlarmReceiver.class);
+                    PendingIntent enablePendingIntent = PendingIntent.getBroadcast(context, 0, enableIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    AlarmManager am = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+                    long millTime = TimeUnit.HOURS.toMillis(currentHour) + TimeUnit.MINUTES.toMillis(currentMin);
+                    am.setExact(am.RTC_WAKEUP, millTime, enablePendingIntent);
+                }
+            }
+        });
+        return view;
+    }
 }
