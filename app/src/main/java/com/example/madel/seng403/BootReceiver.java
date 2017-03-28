0e6011f6655d5ff5e6173901906b3b4fad41572a
mtarannu <mtarannu@ucalgary.ca>
2017-03-27 21:25:59 -0600

    fixed extend for boot receiver so that alarm rings even when phone is at sleep

--- app/src/main/java/com/example/madel/seng403/BootReceiver.java
+++ app/src/main/java/com/example/madel/seng403/BootReceiver.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -20,7 +21,7 @@ import static android.content.Context.ALARM_SERVICE;
  *
  */
 
-public class BootReceiver extends BroadcastReceiver {
+public class BootReceiver extends WakefulBroadcastReceiver {
 
 
     @Override
